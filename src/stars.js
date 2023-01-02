6f89ff8172c3ec245dab1450ae38c3b17b38f21c
kcoderhtml <92754843+kcoderhtml@users.noreply.github.com>
2023-01-02 14:35:53 -0500

    Formated the code with prettier

--- src/stars.js
+++ src/stars.js
@@ -1,21 +1,21 @@
-const symbolsContainer = document.getElementById('symbols');
+const symbolsContainer = document.getElementById("symbols");
 
 setInterval(() => {
-  const symbol = document.createElement('div');
-  symbol.innerHTML = '⚝';
-  symbol.classList.add('symbol');
+	const symbol = document.createElement("div");
+	symbol.innerHTML = "⚝";
+	symbol.classList.add("symbol");
 
-  const x = Math.floor(Math.random() * window.innerWidth);
-  const y = Math.floor(Math.random() * window.innerHeight);
-  const fontSize = Math.floor(Math.random() * 1) + 1; // Random number between 1 and 6
+	const x = Math.floor(Math.random() * window.innerWidth);
+	const y = Math.floor(Math.random() * window.innerHeight);
+	const fontSize = Math.floor(Math.random() * 1) + 1; // Random number between 1 and 6
 
-  symbol.style.left = `${x}px`;
-  symbol.style.top = `${y}px`;
-  symbol.style.fontSize = `${fontSize}rem`;
+	symbol.style.left = `${x}px`;
+	symbol.style.top = `${y}px`;
+	symbol.style.fontSize = `${fontSize}rem`;
 
-  symbolsContainer.appendChild(symbol);
+	symbolsContainer.appendChild(symbol);
 
-  setTimeout(() => {
-    symbol.remove();
-  }, 3000);
+	setTimeout(() => {
+		symbol.remove();
+	}, 3000);
 }, 100);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    screens: {
-      sm: "480px",
-      md: "768px",
-      lg: "976px",
-      xl: "1440px",
-    },
-    extend: {
-      colors: {
-        'mutedpurple-100': "#514d87",
-        'mutedpurple-200': "#353254",
-        'mutedpurple-300': "#282544",
-        'muted-blue-100': "#3c4b60",
-        'muted-blue-200': "#2c3a4f",
-        'muted-blue-300': "#1c293d",
-      },
-    },
-  },
-  plugins: [],
-}
+	content: ["./src/**/*.{html,js}"],
+	theme: {
+		screens: {
+			sm: "480px",
+			md: "768px",
+			lg: "976px",
+			xl: "1440px",
+		},
+		extend: {
+			colors: {
+				"mutedpurple-100": "#514d87",
+				"mutedpurple-200": "#353254",
+				"mutedpurple-300": "#282544",
+				"muted-blue-100": "#3c4b60",
+				"muted-blue-200": "#2c3a4f",
+				"muted-blue-300": "#1c293d",
+			},
+		},
+	},
+	plugins: [],
+};
