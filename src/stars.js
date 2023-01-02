0519f5d0ac7bb0580b0b3650948a8654af12d966
kcoderhtml <92754843+kcoderhtml@users.noreply.github.com>
2023-01-02 12:51:00 -0500

    Update stars.js
    Uncomented rotate lines

--- src/stars.js
+++ src/stars.js
@@ -8,12 +8,12 @@ setInterval(() => {
   const x = Math.floor(Math.random() * window.innerWidth);
   const y = Math.floor(Math.random() * window.innerHeight);
   const fontSize = Math.floor(Math.random() * 6) + 1; // Random number between 1 and 6
-//   const rotation = Math.floor(Math.random() * 360); // Random number between 0 and 360
+  const rotation = Math.floor(Math.random() * 360); // Random number between 0 and 360
 
   symbol.style.left = `${x}px`;
   symbol.style.top = `${y}px`;
   symbol.style.fontSize = `${fontSize}rem`;
-//   symbol.style.transform = `rotate(${rotation}deg)`;
+  symbol.style.transform = `rotate(${rotation}deg)`;
 
   symbolsContainer.appendChild(symbol);
 
