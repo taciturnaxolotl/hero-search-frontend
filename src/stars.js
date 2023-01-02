90a0583bb19a7662e7150bbd99f4b959848da1d5
kcoderhtml <92754843+kcoderhtml@users.noreply.github.com>
2023-01-02 13:01:46 -0500

    Made stars appear again

--- src/stars.js
+++ src/stars.js
@@ -7,17 +7,15 @@ setInterval(() => {
 
   const x = Math.floor(Math.random() * window.innerWidth);
   const y = Math.floor(Math.random() * window.innerHeight);
-  const fontSize = Math.floor(Math.random() * 6) + 1; // Random number between 1 and 6
-  const rotation = Math.floor(Math.random() * 360); // Random number between 0 and 360
+  const fontSize = Math.floor(Math.random() * 1) + 1; // Random number between 1 and 6
 
   symbol.style.left = `${x}px`;
   symbol.style.top = `${y}px`;
   symbol.style.fontSize = `${fontSize}rem`;
-  symbol.style.transform = `rotate(${rotation}deg)`;
 
   symbolsContainer.appendChild(symbol);
 
   setTimeout(() => {
     symbol.remove();
-  }, 1000);
-}, 500);
+  }, 3000);
+}, 10);
